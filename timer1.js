529e8b47cf6c78255cf95b2af0a53cf1db1a7848
Vivian Zhang <vvynz12@gmail.com>
2022-01-18 21:45:06 +0000

    the timer receives an array of numbers from the user via command line and returns a beep at the specified times. The timer will not beep if no numbers are provied, if the input is a negative number and if the input isn't a number

--- timer1.js
+++ timer1.js
@@ -1,5 +1,19 @@
-let timeout = process.argv.slice(2) * 1000;
-// console.log(timeout);
-setTimeout(() => {
-  process.stdout.write("\x07");
-}, timeout);
+function timer() {
+  const numArr = process.argv.slice(2);
+  // input is an array of numbers from the user(command line)
+
+  numArr.forEach((element) => {
+    const delay = Number(element) * 1000;
+
+    // edge cases: if no numbers are given, or if negative number, or if input is not a number; SKIP
+    if (delay === 0 || Number.isNaN(delay) || delay < 0) {
+      console.log("if statement hit");
+      return;
+    } else {
+      setTimeout(() => {
+        process.stdout.write("\x07");
+      }, delay);
+    }
+  });
+}
+timer();
